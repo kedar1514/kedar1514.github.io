bd82feb3db779c6aa9f87ceaa50c21fc0a3e79a2
Kedar Choudhary <46651529+kedar1514@users.noreply.github.com>
2023-03-06 16:44:04 +0530

    Create index.js
    Starting template

---
+++ index.js
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Portfolio</title>
+    <style>
+      /* Add CSS styles here */
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #f1f1f1;
+      }
+      header {
+        background-color: #333;
+        color: white;
+        text-align: center;
+        padding: 50px;
+      }
+      h1 {
+        font-size: 50px;
+      }
+      main {
+        padding: 20px;
+      }
+      .project {
+        background-color: white;
+        margin-bottom: 20px;
+        padding: 20px;
+        border-radius: 10px;
+      }
+      .project h2 {
+        font-size: 30px;
+      }
+      .project p {
+        font-size: 16px;
+        line-height: 1.5;
+      }
+      .project img {
+        width: 100%;
+        border-radius: 10px;
+        margin-bottom: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <header>
+      <h1>My Portfolio</h1>
+    </header>
+    <main>
+      <div class="project">
+        <img src="project1.jpg" alt="Project 1">
+        <h2>Project 1</h2>
+        <p>Description of Project 1</p>
+        <a href="#" target="_blank">View Project</a>
+      </div>
+      <div class="project">
+        <img src="project2.jpg" alt="Project 2">
+        <h2>Project 2</h2>
+        <p>Description of Project 2</p>
+        <a href="#" target="_blank">View Project</a>
+      </div>
+      <div class="project">
+        <img src="project3.jpg" alt="Project 3">
+        <h2>Project 3</h2>
+        <p>Description of Project 3</p>
+        <a href="#" target="_blank">View Project</a>
+      </div>
+    </main>
+  </body>
+</html>
